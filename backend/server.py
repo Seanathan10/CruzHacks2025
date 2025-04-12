f1eb7aaaf6a0bb77cb5a5a423dce7a45c673413b
iconsumeplutonium <uzymc242@outlook.com>
2025-04-11 23:47:36 -0700

    scraper works, partially hooked up api to filter

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,35 @@
-# backend/server.py
-from fastapi import FastAPI
-from backend import news
+from fastapi import FastAPI, Request, HTTPException
+from . import scraper
+import urllib.parse
+# from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+api: FastAPI = FastAPI()
+# api.add_middleware(
+#     CORSMiddleware, 
+#     allow_origins=["*"], 
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-@app.get("/test")
-async def hello():
+@api.get("/courses")
+async def getAllCourses(
+    term: str, 
+    department: str = "", 
+    catalogOp: str = "contains", 
+    catalogNum: str = "", 
+    regStatus: str = "O",
+    titleKeyword: str = "",
+    instructorNameOp: str = "=",
+    instructorName: str = "",
+    ge: str = "",
+    crseUnitsOp: str = "=",
+    crseUnitsFrom: str = "",
+    crseUnitsTo: str = "",
+    crseUnitsExact: str = ""
+):
+    return scraper.queryPisa(term, department, catalogOp, catalogNum, regStatus, titleKeyword, instructorNameOp, instructorName, ge, crseUnitsOp, crseUnitsFrom, crseUnitsTo, crseUnitsExact)
+
+@api.get("/test")
+async def getPath():
     return {"hello": "world"}
-
-app.include_router(news.router)
