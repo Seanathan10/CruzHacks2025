2133cb0932e63774d018623cbb8f1ebef587c167
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2025-04-11 23:57:45 -0700

    router kinda working

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-import RssFeed from './news/news'
+import RssFeed from './news/news';
+import Peak from './peak';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <RssFeed />
-    </>
-  )
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/news">News</Link></li>
+          <li><Link to="/peak">Peak</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+              <img src="/vite.svg" className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+              <img src="/react.svg" className="logo react" alt="React logo" />
+            </a>
+          </div>
+        } />
+        <Route path="/news" element={<RssFeed />} />
+        <Route path="/peak" element={<Peak />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
